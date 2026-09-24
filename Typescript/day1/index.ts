fe15598661b8eb0992a0acc1a58c9cf5711eea0f
satwaj <satwajbachhav2004@gmail.com>
2026-09-25 01:21:30 +0530

    typescript understand

---
+++ Typescript/day1/index.ts
@@ -0,0 +1,54 @@
+// let a: number = 10      // number
+
+// a = 666
+
+// console.log(a)
+
+
+
+// let b: string = "10"   //string
+
+
+// console.log(b)
+
+
+// let arr:[number, string , null, boolean ] = [10, "hello" , null , true]   //tuple
+
+// console.log(arr)
+
+
+// let c: any = 10 //any
+
+// c = "abc"
+
+// console.log(typeof c)
+
+
+// let d: unknown = true //unknown
+// console.log( typeof d)
+
+
+// let f: string | number | boolean = "10"  //union
+
+// f = 10
+// f = true
+
+// console.log(f)
+
+
+
+// let arr:number[] = [10, 12, 100, 10000]  //array
+
+// console.log(arr)
+
+// let arr1:string[] = ["hello", "world"]  //array
+
+// console.log(arr1)
+
+
+// let z: never              //never
+
+// function error(message: string): never {
+//     throw new Error(message);
+// }
+
